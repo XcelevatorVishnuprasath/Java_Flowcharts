3adf882b8016b6ebc6b3e72e46dd1bc895c909e9
Xcelevator-Vishnuprasath <vishnu.2538@xcelevate.org>
2025-08-01 18:33:12 +0530

    Add files via upload

---
+++ Fcp023.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Fcp023
+{
+public static void main(String [] args)
+{
+Scanner scan = new Scanner(System.in);
+int a = scan.nextInt();
+scan.nextLine(); 
+String b = scan.nextLine();
+for (int i=0;i<a;i++)
+{
+System.out.println(b);
+}
+}
+}
